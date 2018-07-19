0b98542f2e6a640539b743e443b2291082c2a837
Roberto Gaitan <rgaitan.it@gmail.com>
2018-07-19 20:25:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,10 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  get '/' do
+  		"Hello World"
+  	end
+  get ('/indexhtml') do
+    erb :indexhtml
+  end
 end
